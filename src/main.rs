a32e3d35bb05da1e7065f99c8c2dee36ef3ba5ea
fraugho <aghoanebom@gmail.com>
2023-12-29 01:22:14 -0900

    Username Support

--- src/main.rs
+++ src/main.rs
@@ -10,32 +10,46 @@ use chrono::{DateTime, Utc}; // Import DateTime and Utc
 // AppState to share among actors
 pub struct AppState {
     messages: Arc<Mutex<Vec<Message>>>,
-    connections: Arc<Mutex<Vec<Addr<MyWs>>>>,
+    connections: Arc<Mutex<Vec<Addr<WsActor>>>>,
 }
 
 impl AppState {
     pub fn broadcast_message(&self, message: String, sender_id: Uuid) {
         let connections = self.connections.lock().unwrap();
         for client in connections.iter() {
-            let _ = client.do_send(WsMessage(message.clone(), sender_id));
+            client.do_send(WsMessage(message.clone(), sender_id));
         }
     }
 }
 
 #[derive(Serialize, Deserialize, Clone)]
 struct Message {
-    id: Option<DateTime<Utc>>, // Make 'id' field optional
+    id: Option<DateTime<Utc>>,
     content: String,
-    sender_id: Uuid,  // Unique identifier for the sender
+    sender_id: Uuid,
+    username: String, // Non-optional username
+}
+
+struct WsMessage(pub String, pub Uuid);
+
+impl actix::Message for WsMessage {
+    type Result = ();
 }
 
 // Define HTTP actor for WebSocket
-struct MyWs {
-    id: Uuid, // Add this line
+struct WsActor {
+    id: Uuid,
+    username: String, // Non-optional username
     state: Arc<AppState>,
 }
 
-impl Actor for MyWs {
+impl WsActor {
+    fn set_username(&mut self, new_username: String) {
+        self.username = new_username;
+    }
+}
+
+impl Actor for WsActor {
     type Context = ws::WebsocketContext<Self>;
 
     fn started(&mut self, ctx: &mut Self::Context) {
@@ -49,50 +63,51 @@ impl Actor for MyWs {
     }
 }
 
-// Internal message for WebSocket text
-struct WsMessage(pub String, pub Uuid);
-
-impl actix::Message for WsMessage {
-    type Result = ();
-}
-
-impl Handler<WsMessage> for MyWs {
+impl Handler<WsMessage> for WsActor {
     type Result = ();
 
     fn handle(&mut self, msg: WsMessage, ctx: &mut Self::Context) {
-        if self.id != msg.1 { // This will work now since `id` exists
+        if self.id != msg.1 {
             ctx.text(msg.0);
         }
     }
 }
 
-// Implement StreamHandler for WebSocket messages
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WsActor {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
         if let Ok(ws::Message::Text(text)) = msg {
             match serde_json::from_str::<Message>(&text) {
                 Ok(mut message) => {
-                    // Set the id here
                     message.id = Some(Utc::now());
-                    match serde_json::to_string(&message) {
-                        Ok(serialized_msg) => {
-                            // Broadcast the message to other clients
-                            self.state.broadcast_message(serialized_msg.clone(), message.sender_id);
-
-                            // Send the message back to the client
-                            //ctx.text(serialized_msg);
-                        },
-                        Err(e) => {
-                            eprintln!("Error serializing message: {:?}", e);
-                        }
+                    // Check if username is empty and set to "None" if it is
+                    if message.username.is_empty() {
+                        message.username = "None".to_string();
                     }
+                    let serialized_msg = serde_json::to_string(&message).unwrap();
+                    self.state.broadcast_message(serialized_msg, message.sender_id);
                 },
-                Err(e) => {
-                    eprintln!("Error deserializing message: {:?}", e);
-                }
+                Err(e) => eprintln!("Error processing message: {:?}", e),
             }
         }
     }
+}
+
+
+async fn ws_index(req: actix_web::HttpRequest, stream: web::Payload, state: web::Data<Arc<AppState>>) -> Result<HttpResponse, actix_web::Error> {
+    ws::start(WsActor { id: Uuid::new_v4(), username: "None".to_string(), state: state.get_ref().clone() }, &req, stream)
+}
+
+async fn send_message(state: web::Data<AppState>, message: web::Json<Message>) -> impl Responder {
+    let msg = message.into_inner();
+
+    let mut messages = state.messages.lock().unwrap();
+    messages.push(msg.clone());
+
+    let serialized_msg = serde_json::to_string(&msg).unwrap();
+
+    state.broadcast_message(serialized_msg, msg.sender_id);
+
+    HttpResponse::Ok().json("Message received")
 }
 
 #[get("/")]
@@ -105,24 +120,6 @@ async fn home_page() -> impl Responder {
             HttpResponse::InternalServerError().finish()
         }
     }
-}
-
-async fn ws_index(req: actix_web::HttpRequest, stream: web::Payload, state: web::Data<Arc<AppState>>) -> Result<HttpResponse, actix_web::Error> {
-    ws::start(MyWs { id: Uuid::new_v4(), state: state.get_ref().clone() }, &req, stream)
-}
-
-async fn send_message(state: web::Data<AppState>, message: web::Json<Message>) -> impl Responder {
-    let msg = message.into_inner();
-
-    let mut messages = state.messages.lock().unwrap();
-    messages.push(msg.clone());
-
-    let serialized_msg = serde_json::to_string(&msg).unwrap();
-
-    // Pass the sender's UUID when broadcasting the message
-    state.broadcast_message(serialized_msg, msg.sender_id);
-
-    HttpResponse::Ok().json("Message received")
 }
 
 #[actix_web::main]
